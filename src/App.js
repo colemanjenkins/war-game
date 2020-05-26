ea25b502cfe427dd373f3f73ccbcb527f8099679
Coleman Jenkins <ctj9qgq@virginia.edu>
2020-05-26 17:27:25 -0400

    progress at end of day

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import CardDisplay from './CardDisplay.js'
 
+// import "antd/dist/antd.css";
+import { Button } from 'antd';
 import { shuffle } from 'lodash';
 
 const valueMapping = new Map([
@@ -45,7 +47,10 @@ class App extends Component {
       gameInProgress: false,
       playerCurrentCard: null,
       computerCurrentCard: null,
-      message: ""
+      message: "",
+      tied: false,
+      playerTieCards: [],
+      computerTieCards: [],
     }
   }
 
@@ -75,39 +80,154 @@ class App extends Component {
     })
   }
 
+  tieActions = () => {
+    this.setState(prevState => {
+      let newPlayerTieCards = [];
+      let newComputerTieCards = [];
+
+      let newPlayerHand = [...prevState.playerHand];
+      let newComputerHand = [...prevState.computerHand];
+
+      let newMessage;
+
+      //take 3 cards from each
+      //if a hand has less than 3 cards
+      //only draw the cards they have left
+      //and compare the last
+      for (let i = 0; i < 3; i++) {
+        if (newPlayerHand.length !== 0)
+          newPlayerTieCards.push(newPlayerHand.pop());
+        if (newComputerHand.length !== 0)
+          newComputerTieCards.push(newComputerHand.pop());
+      }
+
+      //computer wins
+      if (valueMapping.get(newComputerTieCards[newComputerTieCards.length - 1].cardValue) > valueMapping.get(newPlayerTieCards[newPlayerTieCards.length - 1].cardValue)) {
+        newMessage = "You lost!";
+        newComputerHand.push(prevState.computerCurrentCard);
+        newComputerHand.push(prevState.playerCurrentCard);
+        for (let i = 0; i < newComputerTieCards.length; i++) {
+          newComputerHand.push(newComputerTieCards[i]);
+        }
+        for (let i = 0; i < newPlayerTieCards.length; i++) {
+          newComputerHand.push(newPlayerTieCards[i]);
+        }
+        //you win
+      } else if (valueMapping.get(newComputerTieCards[newComputerTieCards.length - 1].cardValue) < valueMapping.get(newPlayerTieCards[newPlayerTieCards.length - 1].cardValue)) {
+        newMessage = "You won!";
+        newPlayerHand.push(prevState.computerCurrentCard);
+        newPlayerHand.push(prevState.playerCurrentCard);
+        for (let i = 0; i < newComputerTieCards.length; i++) {
+          newPlayerHand.push(newComputerTieCards[i]);
+        }
+        for (let i = 0; i < newPlayerTieCards.length; i++) {
+          newPlayerHand.push(newPlayerTieCards[i]);
+        }
+        //another tie
+      } else {
+        return {
+          message: "You tied! Hit play card again to continue",
+          tied: true,
+          computerTieCards: [...prevState.computerTieCards, newComputerTieCards],
+          playerTieCards: [...prevState.playerTieCards, newPlayerTieCards]
+        };
+      }
+
+      return {
+        message: newMessage,
+        tied: false,
+        playerHand: newPlayerHand,
+        computerHand: newComputerHand,
+        computerTieCards: newComputerTieCards,
+        playerTieCards: newPlayerTieCards,
+      };
+    })
+
+  }
+
   playCard = () => {
     this.setState(prevState => {
+      if (prevState.tied) {
+        this.tieActions();
+        return {};
+      }
       let compCards = [...prevState.computerHand];
       let playerCards = [...prevState.playerHand];
       const compCard = compCards.shift();
       const playerCard = playerCards.shift();
-      let message;
+      let newMessage;
 
+      //computer wins
       if (valueMapping.get(compCard.cardValue) > valueMapping.get(playerCard.cardValue)) {
-        message = "You lost!";
+        newMessage = "You lost!";
         compCards.push(compCard);
         compCards.push(playerCard);
-      } else {
-        message = "You won!";
+        //you win
+      } else if (valueMapping.get(compCard.cardValue) < valueMapping.get(playerCard.cardValue)) {
+        newMessage = "You won!";
         playerCards.push(compCard);
         playerCards.push(playerCard);
+        //tie
+      } else {
+        return {
+          message: "You tied! Hit play card again to continue",
+          tied: true,
+          computerCurrentCard: compCard,
+          playerCurrentCard: playerCard,
+          playerHand: playerCards,
+          computerHand: compCards,
+        };
       }
 
-      console.log(playerCard);
-      console.log(compCard);
-      console.log(playerCards);
-      console.log(compCards);
+      //check if game is over
+      if (compCards.length === 0) {
+        this.endGame(true);
+        return {};
+      } else if (playerCards.length === 0) {
+        this.endGame(false);
+        return {};
+      }
+
       return {
         computerCurrentCard: compCard,
         playerCurrentCard: playerCard,
         playerHand: playerCards,
-        computerHand: compCards
+        computerHand: compCards,
+        message: newMessage,
+        computerTieCards: [],
+        playerTieCards: []
       }
     })
   }
 
-  endGame = () => {
-
+  /**
+   * winner === true => player won
+   * winner === false => computer won
+   */
+  endGame = (winner) => {
+    if (winner) {
+      this.setState(prevState => {
+        return {
+          playerHand: [],
+          computerHand: [],
+          gameInProgress: false,
+          playerCurrentCard: null,
+          computerCurrentCard: null,
+          message: "Congrats! You win the game!"
+        }
+      });
+    } else {
+      this.setState(prevState => {
+        return {
+          playerHand: [],
+          computerHand: [],
+          gameInProgress: false,
+          playerCurrentCard: null,
+          computerCurrentCard: null,
+          message: "You got beat by the computer. Better Luck next time."
+        }
+      });
+    }
   }
 
   render() {
@@ -118,27 +238,36 @@ class App extends Component {
         Your deck size: {this.state.playerHand.length} Opponent deck size: {this.state.computerHand.length}
       </div>;
       userOption =
-        <button style={{ justifyContent: "center" }}
-          onClick={() => this.playCard()} > Play Card</ button>;
+        <Button type="primary"
+          onClick={() => this.playCard()} > Play Card</ Button>;
     } else {
       statusBar = null;
       userOption =
-        <button style={{ justifyContent: "center" }}
-          onClick={() => this.startGame()}>Start Game</button>;
+        <Button type="primary"
+          onClick={() => this.startGame()}>Start Game</Button>;
     }
+
+
     return (
       <div className="App" >
-        {statusBar}
+        {this.state.message}
+        <br></br>{statusBar}
         <div className="gameDisplay">
           <CardDisplay title="Your Card"
-            card={this.state.playerCurrentCard} />
+            card={this.state.playerCurrentCard}
+            tieCards={this.state.playerTieCards} />
           <CardDisplay title="Opponent's Card"
-            card={this.state.computerCurrentCard} />
+            card={this.state.computerCurrentCard}
+            tieCards={this.state.computerTieCards} />
         </div>
-        {userOption}
+        <div style={{ textAlign: "center" }}>
+          {userOption}
+        </div>
+
       </div>
     );
   }
+
 }
 
 export default App;
--- src/CardDisplay.js
+++ src/CardDisplay.js
@@ -5,13 +5,20 @@ class CardDisplay extends Component {
     render() {
         const {
             title,
-            card
+            card,
+            tieCards
         } = this.props
         let displayContents;
         if (card !== undefined && card !== null)
             displayContents = card.cardValue + " of " + card.suit;
         else
             displayContents = "";
+
+        if (tieCards.length !== 0) {
+            for (let i = 0; i < tieCards.length; i++) {
+                displayContents += "\n" + tieCards[i].cardValue + " of " + tieCards[i].suit;
+            }
+        }
         return (
             <div style={{ margin: "auto" }}>
                 <h2>{title}</h2>
